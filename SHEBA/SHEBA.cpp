356b9d1fa45b058a8ddb0e797f213a014fe80e90
Phuong <phuong.tran@jvncomm.com>
2020-02-13 13:59:02 -0500

    Add buildatabase.

--- SHEBA/SHEBA.cpp
+++ SHEBA/SHEBA.cpp
@@ -1,39 +1,34 @@
-#include <fmt/format.h>
+﻿#include <fmt/format.h>
 #include <cpprest/http_client.h>
 #include "rapidjson/document.h"
+#include <tabulate/table.hpp>
+#include <concurrent_unordered_map.h>
 
-using namespace utility;                    // Common utilities like string conversions
-using namespace web;                        // Common features like URIs.
-using namespace http;                  // Common HTTP functionality
-using namespace client;          // HTTP client features
-using namespace concurrency::streams;       // Asynchronous streams
+using namespace utility;
+using namespace web;
+using namespace http;
+using namespace client;
+using namespace concurrency::streams;
 
+struct RepoInfo {
+	std::string name;
+	int clones;
+	int uniqueClones;
+	int views;
+	int UniqueViews;
+};
 
-int main()
-{
-	struct RepoInfo {
-		std::string name;
-		int clones;
-		int uniqueClones;
-		int views;
-		int UniqueViews;
-	};
-
-	std::vector<RepoInfo> Infos;
-	web::http::client::http_client client_(U("https://api.github.com/"));
-	
-	std::string user = "phuongtran7";
-
+std::vector<std::string> GetAllPublicRepo(web::http::client::http_client& client, const std::string& user) {
 	// Build request URI and start the request.
 	web::uri_builder builder;
 	builder.set_path(utility::conversions::to_string_t(fmt::format("/users/{}/repos", user)));
 
-	pplx::task<std::vector<std::string>> request_task = client_.request(methods::GET, builder.to_string())
+	pplx::task<std::vector<std::string>> request_task = client.request(methods::GET, builder.to_string())
 		.then([=](http_response response)
 			{
 				if (response.status_code() != status_codes::OK)
 				{
-					fmt::print("Received response status code from Custom Field querry: {}.", response.status_code());
+					fmt::print("Received response status code from get public repo querry: {}.", response.status_code());
 					throw;
 				}
 
@@ -58,10 +53,99 @@ int main()
 
 			try
 			{
-				request_task.wait();
+				return request_task.get();
 			}
 			catch (const std::exception & e)
 			{
 				fmt::print("Error exception: {}", e.what());
 			}
+}
+
+std::vector<RepoInfo> BuildDatabase(web::http::client::http_client& client, const std::string& user, const std::vector<std::string>& input) {
+
+	struct count {
+		int count;
+		int unique;
+	};
+
+	Concurrency::concurrent_unordered_map<std::string, count> views;
+	Concurrency::concurrent_unordered_map<std::string, count> clones;
+
+	std::vector<pplx::task<void>> result;
+
+	for (auto& repo : input) {
+		web::uri_builder view_builder;
+		view_builder.set_path(utility::conversions::to_string_t(fmt::format("/repos/{}/{}/traffic/views", user, repo)));
+
+		pplx::task<void>request_view_task = client.request(methods::GET, view_builder.to_string()).then([=](http_response response)
+			{
+				if (response.status_code() != status_codes::OK)
+				{
+					fmt::print("Received response status code from get view querry: {}.", response.status_code());
+					throw;
+				}
+
+				return response.extract_utf8string();
+			})
+			.then([&](const std::string& json_data)
+				{
+					rapidjson::Document document;
+					document.Parse(json_data.c_str());
+					count temp{ document["count"].GetInt(), document["uniques"].GetInt() };
+					views.insert(std::make_pair(repo, temp));
+				});
+
+		result.push_back(std::move(request_view_task));
+
+
+		web::uri_builder clone_builder;
+		clone_builder.set_path(utility::conversions::to_string_t(fmt::format("/repos/{}/{}/traffic/clones", user, repo)));
+
+		pplx::task<void>request_clone_task = client.request(methods::GET, clone_builder.to_string()).then([=](http_response response)
+			{
+				if (response.status_code() != status_codes::OK)
+				{
+					fmt::print("Received response status code from get clone querry: {}.", response.status_code());
+					throw;
+				}
+
+				return response.extract_utf8string();
+			})
+			.then([&](const std::string& json_data)
+				{
+					rapidjson::Document document;
+					document.Parse(json_data.c_str());
+					count temp{ document["count"].GetInt(), document["uniques"].GetInt() };
+					clones.insert(std::make_pair(repo, temp));
+				});
+
+		result.push_back(std::move(request_clone_task));
+	}
+
+	for (auto& task : result) {
+		task.get();
+	}
+
+	std::vector<RepoInfo> return_val{};
+	for (auto& repo : input) {
+		auto view = views.find(repo)->second;
+		auto clone = views.find(repo)->second;
+		return_val.emplace_back(RepoInfo{repo, view.count, view.unique, clone.count, clone.unique});
+	}
+
+	return return_val;
+}
+
+int main()
+{
+	web::http::client::http_client client_(U("https://api.github.com/"));
+
+
+	std::string user = "phuongtran7";
+
+	auto repos = GetAllPublicRepo(client_, user);
+
+	auto database = BuildDatabase(client_, user, repos);
+
+	std::getchar();
 }
