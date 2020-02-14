38179e52590b4255e82a53159266812b4a54fea2
Phuong <phuong.tran@jvncomm.com>
2020-02-14 10:47:42 -0500

    Use token even for public repo. Refactor make request header. Simplify function calls.

--- SHEBA/SHEBA.cpp
+++ SHEBA/SHEBA.cpp
@@ -4,6 +4,7 @@
 #include <tabulate/table.hpp>
 #include <concurrent_unordered_map.h>
 #include "toml.hpp"
+#include <string>
 
 // Struct to represent each repo and its information
 struct RepoInfo {
@@ -14,8 +15,24 @@ struct RepoInfo {
 	int UniqueViews;
 };
 
-// We don't need the token here as we only care about public repo information anyway.
-auto GetAllPublicRepo(web::http::client::http_client& client, const std::string& user) -> std::vector<std::string> {
+// Commonly used variables
+std::string user{};
+std::string token{};
+
+auto MakeRequest(const utility::string_t& uri) -> web::http::http_request {
+	using namespace utility;
+	using namespace web;
+	using namespace http;
+
+	// Manually craft authentication header to use with GitHub token
+	auto formated_token_string = fmt::format("Token {}", token);
+	http_request request(methods::GET);
+	request.headers().add(L"Authorization", conversions::to_string_t(formated_token_string));
+	request.set_request_uri(uri.c_str());
+	return request;
+}
+
+auto GetAllPublicRepo(web::http::client::http_client& client) -> std::vector<std::string> {
 	using namespace utility;
 	using namespace web;
 	using namespace http;
@@ -24,7 +41,7 @@ auto GetAllPublicRepo(web::http::client::http_client& client, const std::string&
 	web::uri_builder builder;
 	builder.set_path(conversions::to_string_t(fmt::format("/users/{}/repos", user)));
 
-	pplx::task<std::vector<std::string>> request_task = client.request(methods::GET, builder.to_string())
+	pplx::task<std::vector<std::string>> request_task = client.request(MakeRequest(builder.to_string()))
 		.then([&](const http_response& response)
 			{
 				if (response.status_code() != status_codes::OK)
@@ -63,7 +80,7 @@ auto GetAllPublicRepo(web::http::client::http_client& client, const std::string&
 	return std::vector<std::string>{};
 }
 
-auto BuildDatabase(web::http::client::http_client& client, const std::string& user, const std::string& token, const std::vector<std::string>& input) -> std::vector<RepoInfo> {
+auto BuildDatabase(web::http::client::http_client& client, const std::vector<std::string>& input) -> std::vector<RepoInfo> {
 	using namespace utility;
 	using namespace web;
 	using namespace http;
@@ -84,20 +101,13 @@ auto BuildDatabase(web::http::client::http_client& client, const std::string& us
 	// Store all the task so that we can call wait on them later.
 	std::vector<pplx::task<void>> result{};
 
-	auto formated_token_string = fmt::format("Token {}", token);
-
 	// Loop through all our public repo names.
 	for (auto& repo : input) {
 		web::uri_builder view_builder;
 		view_builder.set_path(conversions::to_string_t(fmt::format("/repos/{}/{}/traffic/views", user, repo)));
 
-		// Manually craft authentication header to use with GitHub token
-		http_request view_request(methods::GET);
-		view_request.headers().add(L"Authorization", conversions::to_string_t(formated_token_string));
-		view_request.set_request_uri(view_builder.to_string());
-
 		// Spawn a task to request the view count
-		pplx::task<void>request_view_task = client.request(view_request).then([&](const http_response& response)
+		pplx::task<void>request_view_task = client.request(MakeRequest(view_builder.to_string())).then([&](const http_response& response)
 			{
 				if (response.status_code() != status_codes::OK)
 				{
@@ -121,12 +131,9 @@ auto BuildDatabase(web::http::client::http_client& client, const std::string& us
 
 			web::uri_builder clone_builder;
 			clone_builder.set_path(conversions::to_string_t(fmt::format("/repos/{}/{}/traffic/clones", user, repo)));
-			http_request clone_request(methods::GET);
-			clone_request.headers().add(L"Authorization", conversions::to_string_t(formated_token_string));
-			clone_request.set_request_uri(clone_builder.to_string());
 
 			// Spawn another task for the same repo to request the clone count
-			pplx::task<void>request_clone_task = client.request(clone_request).then([&](const http_response& response)
+			pplx::task<void>request_clone_task = client.request(MakeRequest(clone_builder.to_string())).then([&](const http_response& response)
 				{
 					if (response.status_code() != status_codes::OK)
 					{
@@ -203,10 +210,8 @@ void DrawTable(const std::vector<RepoInfo>& input) {
 	std::cout << data << std::endl;
 }
 
-int main()
+auto main() -> int
 {
-	std::string user{};
-	std::string token{};
 	try {
 		const auto data = toml::parse("Secrets.toml");
 		user = toml::find<std::string>(data, "User");
@@ -219,8 +224,8 @@ int main()
 	}
 
 	web::http::client::http_client client_(U("https://api.github.com/"));
-	auto repos = GetAllPublicRepo(client_, user);
-	auto database = BuildDatabase(client_, user, token, repos);
+	auto repos = GetAllPublicRepo(client_);
+	auto database = BuildDatabase(client_, repos);
 	DrawTable(database);
 	std::getchar();
 }
